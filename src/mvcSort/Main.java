ef840139ce6601a49829393b855a023e5447256c
studentUnk <48737449+studentUnk@users.noreply.github.com>
2021-08-21 10:25:51 -0500

    Prueba búsqueda binaria
    Prueba de búsqueda binaria

--- src/mvcSort/Main.java
+++ src/mvcSort/Main.java
@@ -1,15 +1,18 @@
 package mvcSort;
 
-import algoritmos.QuickSort;
+import algoritmos.*;
 
 public class Main {
 	public static void main(String [] args) {
 		Archivo leerArchivo = new Archivo();
-		leerArchivo.leer("Numeros.txt");
+		//leerArchivo.leer("Numeros.txt");
+		leerArchivo.leer("NumerosOrdenados.txt");
 		//leerArchivo.imprimirDatos();
 		Integer [] lista = leerArchivo.convertirArrayEntero();
-		QuickSort quickSort = new QuickSort();
+		/*QuickSort quickSort = new QuickSort();
 		quickSort.ordenarLista(lista,3);
-		leerArchivo.escribir("NumerosOrdenados.txt",lista);
+		leerArchivo.escribir("NumerosOrdenados.txt",lista);*/
+		BusquedaBinaria bB = new BusquedaBinaria();
+		System.out.println(bB.busquedaBinaria(lista, 520490932));
 	}
 }
