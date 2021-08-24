01b68c4403b30a4b513887922798b57d239feba1
studentUnk <48737449+studentUnk@users.noreply.github.com>
2021-08-23 20:50:34 -0500

    Vista del usuario
    Vista para el usuario haciendo uso de JFrame, JPanel, JButton y demás recursos para que el usuario interactúe con la lista de datos.

--- src/mvcSort/Main.java
+++ src/mvcSort/Main.java
@@ -1,18 +1,24 @@
 package mvcSort;
 
-import algoritmos.*;
+//import algoritmos.*;
+//import mvcSort.Aplicacion;
 
 public class Main {
 	public static void main(String [] args) {
+		Aplicacion aplicacion = new Aplicacion();
+		aplicacion.crearVentana("Lista Ordenada");
+		/*
 		Archivo leerArchivo = new Archivo();
 		//leerArchivo.leer("Numeros.txt");
 		leerArchivo.leer("NumerosOrdenados.txt");
 		//leerArchivo.imprimirDatos();
 		Integer [] lista = leerArchivo.convertirArrayEntero();
-		/*QuickSort quickSort = new QuickSort();
-		quickSort.ordenarLista(lista,3);
-		leerArchivo.escribir("NumerosOrdenados.txt",lista);*/
+		QuickSort quickSort = new QuickSort();
+		quickSort.ordenarLista(lista,1);
+		leerArchivo.escribir("NumerosOrdenados.txt",lista);
 		BusquedaBinaria bB = new BusquedaBinaria();
-		System.out.println(bB.busquedaBinaria(lista, 520490932));
+		System.out.println(bB.busquedaBinaria(lista, 1530796302, 2));
+		*/
+		
 	}
 }
